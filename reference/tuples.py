4426b971d951497a9b953b17a242733d9a40516a
Joy57 <abedinjoynal2@gmail.com>
2018-02-10 23:14:31 -0500

    add tuples

---
+++ reference/tuples.py
@@ -0,0 +1,14 @@
+#Tuples are just immutable lists. Use () instead of []
+x = (1, 2, 3)
+print(len(x))
+
+y = (4, 5, 6)
+print(y[2])
+
+listOfTuples = [x, y]
+print(listOfTuples)
+
+(age, income) = "32,120000".split(',')
+print(age)
+print(income)
+
