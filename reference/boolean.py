72271f5a5fc1a9d31e5e56abf80f7b8ba79698b5
Joy <abedinjoynal2@gmail.com>
2018-02-18 22:57:14 -0500

    boolean

---
+++ reference/boolean.py
@@ -0,0 +1,15 @@
+print (1 == 3)
+#False
+
+print (True or False)
+#True
+
+print (1 is 3)
+#False
+
+if 1 is 3:
+    print ("Really?")
+elif 1 > 3:
+    print("ah nope!")
+else:
+    print ("finally! condition checked!") #will print this as none of the above condition satisfies
